4f22aa1359a7eb224a34b69efbc367d5fb90b417
BuddyChewChew <144280901+BuddyChewChew@users.noreply.github.com>
2025-07-05 13:10:36 -0400

    Update Backup-epg-grabber.py

--- Backup/Backup-epg-grabber.py
+++ Backup/Backup-epg-grabber.py
@@ -76,12 +76,22 @@ def filter_and_build_epg(urls):
 
 
 urls = [
-    'https://epgshare01.online/epgshare01/epg_ripper_ALL_SOURCES1.xml.gz',
     'https://i.mjh.nz/Plex/all.xml.gz',
     'https://i.mjh.nz/SamsungTVPlus/all.xml.gz',
     'https://i.mjh.nz/PlutoTV/all.xml.gz',
     'https://i.mjh.nz/Roku/all.xml.gz',
-    'https://i.mjh.nz/nz/epg.xml.gz'
+    'https://i.mjh.nz/nz/epg.xml.gz',
+    'https://epgshare01.online/epgshare01/epg_ripper_US1.xml.gz',
+    'https://epgshare01.online/epgshare01/epg_ripper_US_LOCALS2.xml.gz',
+    'https://epgshare01.online/epgshare01/epg_ripper_CA1.xml.gz',
+    'https://epgshare01.online/epgshare01/epg_ripper_UK1.xml.gz',
+    'https://epgshare01.online/epgshare01/epg_ripper_AU1.xml.gz',
+    'https://epgshare01.online/epgshare01/epg_ripper_IE1.xml.gz',
+    'https://epgshare01.online/epgshare01/epg_ripper_DE1.xml.gz',
+    'https://epgshare01.online/epgshare01/epg_ripper_ZA1.xml.gz',
+    'https://epgshare01.online/epgshare01/epg_ripper_DUMMY_CHANNELS.xml.gz',
+    'https://epgshare01.online/epgshare01/epg_ripper_US_SPORTS1.xml.gz',
+    'https://epgshare01.online/epgshare01/epg_ripper_FANDUEL1.xml.gz'
 ]
 
 if __name__ == "__main__":
