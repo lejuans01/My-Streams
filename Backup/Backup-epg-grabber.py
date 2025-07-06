e9885c1623f6077b685d8b4736b8a0e300976231
BuddyChewChew <144280901+BuddyChewChew@users.noreply.github.com>
2025-07-06 16:09:18 -0400

    Update Backup-epg-grabber.py

--- Backup/Backup-epg-grabber.py
+++ Backup/Backup-epg-grabber.py
@@ -76,18 +76,8 @@ def filter_and_build_epg(urls):
 
 
 urls = [
-    'https://epgshare01.online/epgshare01/epg_ripper_US1.xml.gz',
-    'https://epgshare01.online/epgshare01/epg_ripper_US_LOCALS2.xml.gz',
-    'https://epgshare01.online/epgshare01/epg_ripper_CA1.xml.gz',
-    'https://epgshare01.online/epgshare01/epg_ripper_UK1.xml.gz',
-    'https://epgshare01.online/epgshare01/epg_ripper_AU1.xml.gz',
-    'https://epgshare01.online/epgshare01/epg_ripper_IE1.xml.gz',
-    'https://epgshare01.online/epgshare01/epg_ripper_DE1.xml.gz',
-    'https://epgshare01.online/epgshare01/epg_ripper_ZA1.xml.gz',
-    'https://epgshare01.online/epgshare01/epg_ripper_DUMMY_CHANNELS.xml.gz',
-    'https://epgshare01.online/epgshare01/epg_ripper_US_SPORTS1.xml.gz',
-    'https://epgshare01.online/epgshare01/epg_ripper_FANDUEL1.xml.gz',
-    'https://epgshare01.online/epgshare01/epg_ripper_PLEX1.xml.gz'
+    'https://epgshare01.online/epgshare01/epg_ripper_ALL_SOURCES1.xml.gz',
+    'https://github.com/matthuisman/i.mjh.nz/raw/refs/heads/master/Roku/all.xml.gz'
 ]
 
 if __name__ == "__main__":
