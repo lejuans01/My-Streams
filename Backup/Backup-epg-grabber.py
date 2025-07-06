c3f7040194e757dabd8742cbc2264126b10b253e
BuddyChewChew <144280901+BuddyChewChew@users.noreply.github.com>
2025-07-06 15:20:31 -0400

    Update Backup-epg-grabber.py

--- Backup/Backup-epg-grabber.py
+++ Backup/Backup-epg-grabber.py
@@ -76,11 +76,6 @@ def filter_and_build_epg(urls):
 
 
 urls = [
-    'https://i.mjh.nz/Plex/all.xml.gz',
-    'https://i.mjh.nz/SamsungTVPlus/all.xml.gz',
-    'https://i.mjh.nz/PlutoTV/all.xml.gz',
-    'https://i.mjh.nz/Roku/all.xml.gz',
-    'https://i.mjh.nz/nz/epg.xml.gz',
     'https://epgshare01.online/epgshare01/epg_ripper_US1.xml.gz',
     'https://epgshare01.online/epgshare01/epg_ripper_US_LOCALS2.xml.gz',
     'https://epgshare01.online/epgshare01/epg_ripper_CA1.xml.gz',
@@ -92,7 +87,7 @@ urls = [
     'https://epgshare01.online/epgshare01/epg_ripper_DUMMY_CHANNELS.xml.gz',
     'https://epgshare01.online/epgshare01/epg_ripper_US_SPORTS1.xml.gz',
     'https://epgshare01.online/epgshare01/epg_ripper_FANDUEL1.xml.gz',
-    'https://epgshare01.online/epgshare01/epg_ripper_PLEX1.xml.gz
+    'https://epgshare01.online/epgshare01/epg_ripper_PLEX1.xml.gz'
 ]
 
 if __name__ == "__main__":
