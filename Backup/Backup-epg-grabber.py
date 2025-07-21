21171f891920516e234a131fcc5979a3a699288e
BuddyChewChew <144280901+BuddyChewChew@users.noreply.github.com>
2025-07-21 14:21:13 -0400

    Update Backup-epg-grabber.py

--- Backup/Backup-epg-grabber.py
+++ Backup/Backup-epg-grabber.py
@@ -89,7 +89,7 @@ urls = [
     'https://epgshare01.online/epgshare01/epg_ripper_FANDUEL1.xml.gz',
     'https://raw.githubusercontent.com/matthuisman/i.mjh.nz/refs/heads/master/Plex/all.xml',
     'https://raw.githubusercontent.com/matthuisman/i.mjh.nz/refs/heads/master/PlutoTV/all.xml',
-    'https://github.com/matthuisman/i.mjh.nz/raw/master/PlutoTV/us.xml.gz',
+    'https://github.com/matthuisman/i.mjh.nz/raw/master/Roku/all.xml.gz',
     'https://github.com/BuddyChewChew/xumo-playlist-generator/raw/refs/heads/main/playlists/xumo_epg.xml.gz',
     'https://epgshare01.online/epgshare01/epg_ripper_PLEX1.xml.gz'
 ]
