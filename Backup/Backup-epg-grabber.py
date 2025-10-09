124f05ce6fc7d80c565ea2363a74ec91e0ecc78c
BuddyChewChew <144280901+BuddyChewChew@users.noreply.github.com>
2025-10-09 14:56:10 -0400

    Update Backup-epg-grabber.py

--- Backup/Backup-epg-grabber.py
+++ Backup/Backup-epg-grabber.py
@@ -76,10 +76,8 @@ def filter_and_build_epg(urls):
 
 
 urls = [
-    'https://epgshare01.online/epgshare01/epg_ripper_US1.xml.gz',
     'https://epgshare01.online/epgshare01/epg_ripper_US2.xml.gz',
     'https://epgshare01.online/epgshare01/epg_ripper_US_LOCALS1.xml.gz',
-    'https://epgshare01.online/epgshare01/epg_ripper_US_LOCALS2.xml.gz',
     'https://epgshare01.online/epgshare01/epg_ripper_CA1.xml.gz',
     'https://epgshare01.online/epgshare01/epg_ripper_CA2.xml.gz'
     'https://epgshare01.online/epgshare01/epg_ripper_UK1.xml.gz',
